f79dac8be29fdd403f68793e44d0cd3bb90474fe
Shiyamalan <rsrishiya@gmail.com>
2017-04-29 11:40:59 +0800

    It is business model to store the contacts.

---
+++ src/main/Java/sg/dbsys/server/models/Contact.java
@@ -0,0 +1,37 @@
+package sg.dbsys.server.models;
+
+public class Contact {
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	
+	private int id;
+
+	private String firstName;
+
+	private String lastName;
+
+}
---
+++ src/main/Java/sg/dbsys/server/models/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains Business Models (Mainly contains Database Entity classes)
+ * @author shiyam
+ * @version 1.0
+ * @since 1.0
+ */
+package sg.dbsys.server.models;
